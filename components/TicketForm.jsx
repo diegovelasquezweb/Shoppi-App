06be9d95f47bd659d85dfdd1f2066833b2c07ab3
Diego Velasquez <contacto@diegovelasquez.co>
2023-12-08 14:23:54 -0500

    add mapview

--- components/TicketForm.jsx
+++ components/TicketForm.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, TextInput, Button, Image, ToastAndroid, Platform, TouchableOpacity } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Location from 'expo-location';
+import MapView, { Marker } from 'react-native-maps';
 
 import { getFirestore, addDoc, collection } from 'firebase/firestore';
 import { firestore } from '../firebaseConfig';
@@ -19,6 +20,20 @@ const TicketForm = ({ route }) => {
 
   const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
 
+  const [isMapVisible, setMapVisibility] = useState(false);
+  const [selectedLocation, setSelectedLocation] = useState(null);
+
+  const handleLocationSelect = (event) => {
+    setSelectedLocation({
+      latitude: event.nativeEvent.coordinate.latitude,
+      longitude: event.nativeEvent.coordinate.longitude,
+    });
+  };
+
+  const handleChangeLocation = () => {
+    setMapVisibility(true);
+  };
+
 
   useEffect(() => {
     (async () => {
@@ -32,6 +47,7 @@ const TicketForm = ({ route }) => {
       let reverseGeocode = await Location.reverseGeocodeAsync(location.coords);
       if (reverseGeocode && reverseGeocode.length > 0) {
         setLocation(reverseGeocode[0].city + ', ' + reverseGeocode[0].country);
+        setSelectedLocation(location.coords);
       }
     })();
   }, []);
@@ -66,6 +82,16 @@ const TicketForm = ({ route }) => {
     setDatePickerVisibility(true);
   };
 
+  const handleSetNewLocation = async () => {
+    if (selectedLocation) {
+      let reverseGeocode = await Location.reverseGeocodeAsync(selectedLocation);
+      if (reverseGeocode && reverseGeocode.length > 0) {
+        setLocation(reverseGeocode[0].city + ', ' + reverseGeocode[0].country);
+        setMapVisibility(false);
+      }
+    }
+  };
+
   return (
     <View>
       <Text style={styles.text}>Create Ticket</Text>
@@ -84,6 +110,37 @@ const TicketForm = ({ route }) => {
         multiline
         numberOfLines={5}
       />
+      <TextInput
+        style={styles.input}
+        value={location}
+        placeholder="Location"
+        editable={false}
+      />
+      <Button title="Change Location" onPress={handleChangeLocation} />
+      {isMapVisible && (
+        <View>
+          <MapView
+            style={{ width: '100%', height: 200 }}
+            initialRegion={{
+              latitude: selectedLocation ? selectedLocation.latitude : 37.78825,
+              longitude: selectedLocation ? selectedLocation.longitude : -122.4324,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+            onPress={handleLocationSelect}
+          >
+            {selectedLocation && (
+              <Marker
+                coordinate={{
+                  latitude: selectedLocation.latitude,
+                  longitude: selectedLocation.longitude,
+                }}
+              />
+            )}
+          </MapView>
+          <Button title="Set New Location" onPress={handleSetNewLocation} />
+        </View>
+      )}
       <TakePhoto onPhotoTaken={(image) => setPhoto(image)} />
       <TouchableOpacity onPress={showDatePicker}>
         <TextInput
@@ -101,12 +158,7 @@ const TicketForm = ({ route }) => {
           onChange={handleDateChange}
         />
       )}
-      <TextInput
-        style={styles.input}
-        value={location}
-        placeholder="Location"
-        editable={false}
-      />
+
       <View style={styles.buttonContainer}>
         <Button title="Submit" onPress={handleCreateTicket} />
       </View>
